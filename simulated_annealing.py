e14411e7756865f89b17b901f912cf82aeadde8a
Roverato Chiara <chiara.roverato@student-cs.fr>
2023-12-06 16:41:47 +0100

    update

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,81 +1,62 @@
-from numpy import *
-from random import *
-from matplotlib import pyplot
-from numpy.random import *
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+import math
+from Utils import modularity
 
-# objective function
-def objective(x):
-	return 0
+def simulated_annealing(graph, initial_partition, temperature, cooling_rate, iterations):
+    current_partition = initial_partition.copy()
+    current_score = modularity(graph, current_partition)
 
-# define range for input
-bounds = asarray([[-5.0, 5.0]])
+    best_partition = current_partition.copy()
+    best_score = current_score
 
-# simulated annealing algorithm
-def simulated_annealing(objective, bounds, n_iterations, step_size, temp):
-	# generate an initial point
-	best = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
-	# evaluate the initial point
-	best_eval = objective(best)
-	# current working solution
-	curr, curr_eval = best, best_eval
-	# run the algorithm
-	for i in range(n_iterations):
-		# take a step
-		candidate = curr + randn(len(bounds)) * step_size
-		# evaluate candidate point
-		candidate_eval = objective(candidate)
-		# check for new best solution
-		if candidate_eval < best_eval:
-			# store new best point
-			best, best_eval = candidate, candidate_eval
-			# report progress
-			print('>%d f(%s) = %.5f' % (i, best, best_eval))
-		# difference between candidate and current point evaluation
-		diff = candidate_eval - curr_eval
-		# calculate temperature for current epoch
-		t = temp / float(i + 1)
-		# calculate metropolis acceptance criterion
-		metropolis = exp(-diff / t)
-		# check if we should keep the new point
-		if diff < 0 or rand() < metropolis:
-			# store the new current point
-			curr, curr_eval = candidate, candidate_eval
-	return [best, best_eval]
+    for _ in range(iterations):
+        new_partition = perturb_partition(current_partition)
+        new_score = modularity(graph, new_partition)
 
-# convex unimodal optimization function
+        if new_score > current_score or random.random() < math.exp((new_score - current_score) / temperature):
+            current_partition = new_partition
+            current_score = new_score
+
+        if new_score > best_score:
+            best_partition = new_partition
+            best_score = new_score
+
+        temperature *= 1 - cooling_rate
+
+    return best_partition, best_score
+
+def perturb_partition(partition):
+    perturbed_partition = partition.copy()
+    node = random.choice(list(partition.keys()))
+    
+    # Vérifier si l'ensemble de communautés est vide
+    available_communities = set(partition.values()) - {partition[node]}
+    if not available_communities:
+        new_community = random.choice(list(set(partition.values())))
+    else:
+        new_community = random.choice(list(available_communities))
+    
+    perturbed_partition[node] = new_community
+    return perturbed_partition
 
 
-# objective function
-def objective(x):
-	return x[0]**2.0
+# Exemple d'utilisation
+if __name__ == "__main__":
+    path="Datasets/CA-HepTh.txt"
+    G= nx.read_edgelist(path, comments='#',delimiter='\t') 
+    # Initialiser une partition initiale
+    initial_partition = {node: random.randint(0,11) for node in G.nodes()}
 
-# define range for input
-r_min, r_max = -5.0, 5.0
-# sample input range uniformly at 0.1 increments
-inputs = arange(r_min, r_max, 0.1)
-# compute targets
-results = [objective([x]) for x in inputs]
-# create a line plot of input vs result
-pyplot.plot(inputs, results)
-# define optimal input value
-x_optima = 0.0
-# draw a vertical line at the optimal input
-pyplot.axvline(x=x_optima, ls='--', color='red')
-# show the plot
-pyplot.show()
+    # Paramètres du Simulated Annealing
+    initial_temperature = 1.0
+    cooling_rate = 0.01
+    iterations = 100
 
-# explore temperature vs algorithm iteration for simulated annealing
-from matplotlib import pyplot
-# total iterations of algorithm
-iterations = 100
-# initial temperature
-initial_temp = 10
-# array of iterations from 0 to iterations - 1
-iterations = [i for i in range(iterations)]
-# temperatures for each iterations
-temperatures = [initial_temp/float(i + 1) for i in iterations]
-# plot iterations vs temperatures
-pyplot.plot(iterations, temperatures)
-pyplot.xlabel('Iteration')
-pyplot.ylabel('Temperature')
-pyplot.show()
+    # Appliquer le Simulated Annealing
+    final_partition, final_score = simulated_annealing(G, initial_partition, initial_temperature, cooling_rate, iterations)
+
+    # Afficher les résultats
+    print("Final Partition:", final_partition)
+    print("Final Modularity Score:", final_score)
